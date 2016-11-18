63ce5e1a307929219a662916c191dbe77a2e841a
Salvatore Skare <skare.salvato@uwlax.edu>
2016-11-18 11:18:42 -0600

    tryiing some changes to json

--- main.cpp
+++ main.cpp
@@ -73,8 +73,8 @@ static int callback_roombot(struct lws *wsi,
         
         //encode everything in JSON
         char* json = (char*) malloc(strlen(img) + 255);
-        sprintf(json, "{\"robotdata\":\"webcam\":\"%s\",\"vac\":%s,\"distance\":%d}", img, isVacOn(), readSensor());
-        printf("%s\n", json);
+        sprintf(json, "{\"webcam\":\"%s\",\"vac\":%s,\"distance\":%d}", img, isVacOn(), readSensor());
+
         unsigned char *buf = (unsigned char*) malloc(LWS_SEND_BUFFER_PRE_PADDING + strlen(json) + LWS_SEND_BUFFER_POST_PADDING);
 
         //write image to client
@@ -98,7 +98,7 @@ static struct lws_protocols protocols[] = {
         "roombot-protocol", // protocol name - very important!
         callback_roombot,   // callback
         0,                         // we don't use any per session data
-        4096
+        16384
     },
     {
         NULL, NULL, 0   /* End of list */
